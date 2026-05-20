6afa5aeee1b2ee8c2ac8d41ccc3410b66cc17a67
MiguelD <migueldoliveira142@gmail.com>
2026-05-20 19:39:23 +0100

    Completed Phase 1 of 7th JS Exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  // Converts the string to an array
+  const strToArray = str.split('');
 
+  // Reverses the array order
+  const reversedArray = strToArray.reverse('');
+
+  // Joins the reversed array
+  const finalStr = reversedArray.join('');
+  
+  return finalStr;
 };
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
