6ad2ea7edb3946e2af9e3f34d4b9e460e4c1c8be
MiguelD <migueldoliveira142@gmail.com>
2026-05-20 21:23:14 +0100

    Completed Phase 1 of 6th JS Exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  var repeatedStr = "";
+  if (num === 0) {
+    return ('ERROR');
+  }
+  // While the number is greater than 0, this will run
+  while(num > 0) {
+    repeatedStr += string;
+    num--;
+  }
+
+  return repeatedStr;
+ 
 };
+
+  
+
+console.log(repeatString('hey', 1));
 
 // Do not edit below this line
 module.exports = repeatString;
