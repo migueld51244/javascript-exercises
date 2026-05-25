55d8f92725fab7727f93afe62166d66b252e8566
unknown <migueldoliveira142@gmail.com>
2026-05-25 13:51:32 +0100

    Completed Phase 1 of JS Exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    const itemIndex = arr.indexOf(num);
+    const removed = arr.splice(itemIndex, 1);
+    return arr;
 };
+
+const array = [1, 2, 3, 4]
+
+console.log(removeFromArray(array, 1));
 
 // Do not edit below this line
 module.exports = removeFromArray;
