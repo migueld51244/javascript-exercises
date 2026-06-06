d6453672c94bee9866aee2b551063bf123f54126
MiguelD <migueldoliveira142@gmail.com>
2026-06-06 07:37:25 +0100

    Completed 9th exercise of fundations JS exercises

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,11 +1,20 @@
 const sumAll = function(num1, num2) {
-  let total = 0
-  for (num1; num1 <= num2; num1++) {
-    total += num1
+  let total = 0;
+  let min = Math.min(num1, num2)
+  let max = Math.max(num1, num2)
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  } else if (num1 % !0 || num2 % !0) {
+    return 'ERROR';
+  } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return 'ERROR';
+  }
+  for (min; min <= max; min++) {
+    total += min;
   }
   return total;
 };
 
-console.log(sumAll(1,3));
+console.log(sumAll("2",1));
 // Do not edit below this line
 module.exports = sumAll;
