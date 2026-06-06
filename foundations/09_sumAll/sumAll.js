17cea1e871acca1b579d1bf0d750c8df2b7b3e1f
MiguelD <migueldoliveira142@gmail.com>
2026-06-06 07:05:11 +0100

    Completed Phase 1 of 9th JS Exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let total = 0
+  for (num1; num1 <= num2; num1++) {
+    total += num1
+  }
+  return total;
 };
 
+console.log(sumAll(1,3));
 // Do not edit below this line
 module.exports = sumAll;
