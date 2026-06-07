af72ba88a4c66b576ad42715a981193bab70be26
MiguelD <migueldoliveira142@gmail.com>
2026-06-07 07:50:03 +0100

    Completed 11th exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let converted = 0;
+  let formula = ((fahrenheit - 32) * (5/9));
+  converted = formula;
+  let rounded = Math.round(converted * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100));
+
+const convertToFahrenheit = function(celcius) {
+  let converted = 0;
+  let formula = (celcius * 1.8 + 32);
+  converted = formula;
+  let rounded = Math.round(converted * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
