3fb522a5cd2d800448bbfea6858a46720622edac
MiguelD <migueldoliveira142@gmail.com>
2026-06-22 11:00:22 +0100

    Completed 12 Exercise Calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+ const sumArray = arr.reduce((total, current) => total + current, 0);
+ return sumArray;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, accumulated) => total * accumulated, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+    return -1;
+  } else if(num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
