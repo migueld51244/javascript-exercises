6ec0cfacffd8f28a3764b5ab1a64b53111bce7d9
MiguelD <migueldoliveira142@gmail.com>
2026-06-24 11:31:53 +0100

    Passed 16th exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+const getAge = function(person) {
+  if (person.yearOfDeath) {
+    return person.yearOfDeath - person.yearOfBirth;
+  } else {
+    return new Date().getFullYear() - person.yearOfBirth;
+  }
+};
 
+    const oldest = people.reduce((accumulator, current) => {
+  const currentAge = getAge(current);
+  const accumulatorAge = getAge(accumulator);
+  return currentAge > accumulatorAge ? current : accumulator;
+});
+    return oldest;
 };
 
 // Do not edit below this line
