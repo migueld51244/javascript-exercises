a9442ebed6406c62b1491d1fa21e8a60175bc43b
MiguelD <migueldoliveira142@gmail.com>
2026-06-24 10:34:00 +0100

    Completed 15th JS exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,4 +1,16 @@
 const getTheTitles = function() {
+  const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+  return books.map((book) => {return book.title;});
 
 };
 
