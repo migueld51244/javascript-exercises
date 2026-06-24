9edf9fa3ec7a373752f2a590880c5a868bd224e5
MiguelD <migueldoliveira142@gmail.com>
2026-06-24 10:11:03 +0100

    Completed 13th JS Exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strLowerCase = str.toLowerCase();
+  const strNoSpaces = strLowerCase.replaceAll(" ", "");
+  const strNoPunctuation = strNoSpaces.replaceAll(/[^a-zA-Z0-9]/g, "");
+  const string = strNoPunctuation;
 
+  const reversedStr = string.split('').reverse().join('');
+
+  if(string === reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
